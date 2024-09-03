2d80373e8b421dbf0e3b95737f9617929d09eef4
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-03 19:52:58 +0000

    update blog page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,16 @@
-// pages/index.js or pages/HomePage.js
-import React from "react";
+"use client";
+
+import * as React from "react";
+import { useState } from "react";
 import { Container, Grid } from "@mui/material";
 import MostRecentPost from "../components/MostRecentPost";
 import FeaturedPost from "../components/FeaturedPost";
 import { useHomePage } from "../contexts/HomePageContext";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../components/Header";
+import Footer from "./Footer";
 
 const HomePage = () => {
   const { mostRecentPost, featuredPosts, error } = useHomePage();
@@ -12,15 +19,49 @@ const HomePage = () => {
     return <p>Error loading content: {error.message}</p>;
   }
 
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [isDarkMode, setIsDarkMode] = useState(prefersDarkMode);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: isDarkMode ? "dark" : "light",
+        },
+        typography: {
+          fontFamily: '"Georgia", "Roboto", sans-serif',
+        },
+      }),
+    [isDarkMode]
+  );
+
   return (
-    <Container maxWidth="lg">
-      <MostRecentPost post={mostRecentPost} />
-      <Grid container spacing={4}>
-        {featuredPosts.map((post) => (
-          <FeaturedPost key={post.id} post={post} />
-        ))}
-      </Grid>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="64">
+        <Header
+          title="Prop News"
+          toggleTheme={toggleTheme}
+          isDarkMode={isDarkMode}
+        />
+        <main>
+          <MostRecentPost post={mostRecentPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+        </main>
+      </Container>
+      <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+      />
+    </ThemeProvider>
   );
 };
 
