60f2f776cbd04a9bbb4e8164dfcdeb363d8938ba
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-03 20:46:03 +0000

    update blog page

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -1,15 +1,15 @@
-// components/FeaturedPost.js
-import React from "react";
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  Button,
-} from "@mui/material";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import { useRouter } from "next/router";
+import Button from "@mui/material";
 
-const FeaturedPost = ({ post }) => {
+function FeaturedPost({ post }) {
   const router = useRouter();
 
   const handleClick = () => {
@@ -17,26 +17,58 @@ const FeaturedPost = ({ post }) => {
   };
 
   return (
-    <Card sx={{ display: "flex", flexDirection: "column", maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={post.image}
-        alt={post.title}
-      />
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {post.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {post.body.slice(0, 100)}...
-        </Typography>
-        <Button onClick={handleClick} sx={{ mt: 2 }}>
-          Read more
-        </Button>
-      </CardContent>
-    </Card>
+    <Grid item xs={12} md={6}>
+      <CardActionArea component="a" href="#">
+        <Card
+          sx={{
+            display: "flex",
+            bgcolor: "#02353C",
+            color: "white",
+            maxHeight: 200,
+          }}
+        >
+          <CardContent
+            sx={{
+              flex: 1,
+              padding: "8px",
+            }}
+          >
+            <Typography component="h2" variant="h5">
+              {post.title}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              sx={{ color: "#B0B0B0", fontSize: "0.8rem" }}
+            >
+              {post.date}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {post.description}
+            </Typography>
+          </CardContent>
+          <Button onClick={handleClick} sx={{ mt: 2 }}>
+            Read more
+          </Button>
+          <CardMedia
+            component="img"
+            sx={{ width: 120, height: "auto" }} // Adjust width and let height adjust automatically
+            image={post.image}
+            alt={post.imageLabel}
+          />
+        </Card>
+      </CardActionArea>
+    </Grid>
   );
+}
+
+FeaturedPost.propTypes = {
+  post: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    imageLabel: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default FeaturedPost;
--- src/components/MostRecentPost.js
+++ src/components/MostRecentPost.js
@@ -1,54 +1,94 @@
-import React from "react";
-import { Typography, Box, Button } from "@mui/material";
-import { useRouter } from "next/router";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Link from "next/link"; // Use Next.js Link for client-side navigation
+import Box from "@mui/material/Box";
 
-const MostRecentPost = ({ post }) => {
-  const router = useRouter();
-
+function MostRecentPost({ post }) {
   // Ensure that `post` and its properties are defined before attempting to use them
   if (!post || !post.image || !post.title || !post.description || !post.link) {
     return null; // Render nothing if the post data is incomplete
   }
 
-  const handleClick = () => {
-    router.push(post.link);
-  };
-
   return (
-    <Box
+    <Paper
       sx={{
         position: "relative",
-        height: 400,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundImage: `url(${post.image})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
+        backgroundColor: "grey.800",
         color: "#fff",
-        textAlign: "center",
-        p: 4,
-        borderRadius: 2,
-        boxShadow: 2,
+        mb: 4,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundImage: `url(${post.image})`,
       }}
     >
-      <Typography variant="h3" component="h1" gutterBottom>
-        {post.title}
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        {post.description.slice(0, 150)}...
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleClick}
-        sx={{ mt: 2 }}
-      >
-        {post.linkText}
-      </Button>
-    </Box>
+      {/* Increase the priority of the hero background image */}
+      {
+        <img
+          style={{ display: "none" }}
+          src={post.image}
+          alt={post.imageText}
+        />
+      }
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: "rgba(0,0,0,.3)",
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: "relative",
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography
+              component="h1"
+              variant="h3"
+              color="inherit"
+              gutterBottom
+            >
+              {post.title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {post.description}
+            </Typography>
+            <Link href={post.link} passHref>
+              <Typography
+                variant="subtitle1"
+                color="inherit"
+                component="a"
+                sx={{ textDecoration: "underline", cursor: "pointer" }}
+              >
+                {post.linkText || "Read More"}
+              </Typography>
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
   );
+}
+
+MostRecentPost.propTypes = {
+  post: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    imageText: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    linkText: PropTypes.string,
+  }).isRequired,
 };
 
 export default MostRecentPost;
