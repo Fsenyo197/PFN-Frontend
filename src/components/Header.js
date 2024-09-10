94d00c08026e2bd8c4298c199287592ba621a0fa
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-10 20:10:48 +0000

    Modify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,13 @@ import { useTheme } from "@mui/material/styles";
 import { useHeader } from "../contexts/HeaderContext";
 
 function Header() {
-  const { categories, isDarkMode, toggleTheme } = useHeader();
+  const { isDarkMode, toggleTheme } = useHeader();
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const headerTextColor = isDarkMode ? "#ffffff" : "#02353C";
+
+  const categories = ["News", "Prices", "Payouts", "Rules", "Trading Platform"];
 
   return (
     <React.Fragment>
@@ -36,7 +38,7 @@ function Header() {
           noWrap
           sx={{ flexGrow: 1 }}
         >
-          Prop News
+          Prop Firm News
         </Typography>
         <IconButton>
           <SearchIcon sx={{ color: headerTextColor }} />
@@ -45,30 +47,29 @@ function Header() {
           {isDarkMode ? "Light Mode" : "Dark Mode"}
         </Button>
       </Toolbar>
-      {categories && categories.length > 0 && (
-        <Toolbar
-          component="nav"
-          variant="dense"
-          sx={{
-            justifyContent: "space-between",
-            overflowX: "auto",
-            bgcolor: isDarkMode ? "#000000" : "#ffffff",
-          }}
-        >
-          {categories.map((category) => (
-            <Link
-              color={headerTextColor}
-              noWrap
-              key={category.name}
-              variant="body2"
-              href={`/categories/${category.name.toLowerCase()}`}
-              sx={{ p: 1, flexShrink: 0 }}
-            >
-              {category.name}
-            </Link>
-          ))}
-        </Toolbar>
-      )}
+
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{
+          justifyContent: "space-between",
+          overflowX: "auto",
+          bgcolor: isDarkMode ? "#000000" : "#ffffff",
+        }}
+      >
+        {categories.map((category) => (
+          <Link
+            color={headerTextColor}
+            noWrap
+            key={category}
+            variant="body2"
+            href={`/${category.toLowerCase().replace(/\s+/g, "-")}`}
+            sx={{ p: 1, flexShrink: 0 }}
+          >
+            {category}
+          </Link>
+        ))}
+      </Toolbar>
     </React.Fragment>
   );
 }
