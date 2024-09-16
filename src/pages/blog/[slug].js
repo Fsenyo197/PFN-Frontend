aa67b95ad6956a93ba8f5a2bf43327f60594566d
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-16 21:13:12 +0000

    Add footer

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -88,77 +88,89 @@ const BlogPost = () => {
   }
 
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", p: 2 }}>
-      {/* Back Icon */}
-      <Box
-        sx={{
-          position: "fixed",
-          top: 16,
-          left: 16,
-          zIndex: 1000,
-        }}
-      ></Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      {/* Main Blog Post Content */}
+      <Box sx={{ flexGrow: 1, p: 2 }}>
+        {/* Back Icon */}
+        <Box
+          sx={{
+            position: "fixed",
+            top: 16,
+            left: 16,
+            zIndex: 1000,
+          }}
+        ></Box>
 
-      {/* Drawer for Navigation */}
-      <Box sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1000 }}>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={toggleDrawer}
-          sx={{ ml: 2 }}
+        {/* Drawer for Navigation */}
+        <Box
+          sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1000 }}
         >
-          <MenuIcon />
-        </IconButton>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawer}
+            sx={{ ml: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+          <List>
+            <ListItem button onClick={() => handleNavigation("/")}>
+              <ListItemText primary="Home" />
+            </ListItem>
+            {categories.map((category) => (
+              <ListItem
+                button
+                key={category}
+                onClick={() =>
+                  handleNavigation(`/categories/${category.toLowerCase()}`)
+                }
+              >
+                <ListItemText primary={category} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+
+        <Paper sx={{ maxWidth: 800, p: 3 }}>
+          <Typography variant="h4" gutterBottom>
+            {article.title}
+          </Typography>
+
+          {/* Blog Post Image */}
+          {article.image && (
+            <Box
+              component="img"
+              src={article.image}
+              alt={article.title}
+              sx={{
+                width: "100%",
+                height: "auto",
+                borderRadius: 2,
+                mt: 2,
+                mb: 4,
+              }}
+            />
+          )}
+
+          <Box
+            sx={{ mt: 2 }}
+            dangerouslySetInnerHTML={{ __html: article.body }}
+          />
+        </Paper>
       </Box>
 
-      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
-        <List>
-          <ListItem button onClick={() => handleNavigation("/")}>
-            <ListItemText primary="Home" />
-          </ListItem>
-          {categories.map((category) => (
-            <ListItem
-              button
-              key={category}
-              onClick={() =>
-                handleNavigation(`/categories/${category.toLowerCase()}`)
-              }
-            >
-              <ListItemText primary={category} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-
-      {/* Main Blog Post Content */}
-      <Paper sx={{ maxWidth: 800, p: 3 }}>
-        <Typography variant="h4" gutterBottom>
-          {article.title}
-        </Typography>
-
-        {/* Blog Post Image */}
-        {article.image && (
-          <Box
-            component="img"
-            src={article.image}
-            alt={article.title}
-            sx={{
-              width: "100%",
-              height: "auto",
-              borderRadius: 2,
-              mt: 2,
-              mb: 4,
-            }}
-          />
-        )}
-
-        <Box
-          sx={{ mt: 2 }}
-          dangerouslySetInnerHTML={{ __html: article.body }}
-        />
-      </Paper>
-      {/* Add Footer here */}
+      {/* Footer */}
       <Footer
         title="Footer Title"
         description="Some description about the blog"
