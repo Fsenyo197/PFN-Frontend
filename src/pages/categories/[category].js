c4810d4b2cd25f785a7bed700050a99265ed11c8
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-21 22:45:29 +0000

    category

--- src/pages/categories/[category].js
+++ src/pages/categories/[category].js
@@ -61,7 +61,7 @@ const CategoryPage = ({ category, articles }) => {
 
   return (
     <>
-      <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
+      <div style={{ maxWidth: "1200px" }}>
         {/* Back and Home icons with space between */}
         <div
           style={{
