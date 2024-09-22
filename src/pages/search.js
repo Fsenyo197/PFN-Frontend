d49f7573c949e1481205c71b038f23b8e7820bff
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-22 00:06:40 +0000

    Add a search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,15 +5,18 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import FeaturedPost from "../components/FeaturedPost";
 import FetchArticles from "../utils/FetchArticles";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const SearchResultsPage = () => {
   const router = useRouter();
   const { query } = router.query || {};
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true); // Loading state
 
   useEffect(() => {
     if (query) {
       const fetchAndFilterArticles = async () => {
+        setLoading(true); // Set loading to true when starting to fetch
         try {
           const articles = await FetchArticles(); // Fetch all articles
 
@@ -29,20 +32,26 @@ const SearchResultsPage = () => {
           setResults(filteredResults);
         } catch (error) {
           console.error("Error fetching search results:", error);
+        } finally {
+          setLoading(false); // Set loading to false after fetching
         }
       };
 
       fetchAndFilterArticles();
+    } else {
+      setLoading(false); // Set loading to false if there's no query
     }
   }, [query]);
 
   return (
-    <Container sx={{ marginTop: 4 }}>
+    <Container>
       <Typography variant="h4" gutterBottom>
         Search Results for: &quot;{query}&quot;
       </Typography>
 
-      {results.length > 0 ? (
+      {loading ? (
+        <CircularProgress />
+      ) : results.length > 0 ? (
         <Grid container spacing={4}>
           {results.map((post, index) => (
             <FeaturedPost key={index} post={post} />
