8343afaa7ccc98f1a23add4571ffb01c59e11373
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-23 23:39:13 +0000

    Add header to category page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { HeaderProvider } from "../contexts/HeaderContext";
 import { HomePageProvider } from "../contexts/HomePageContext";
+import "../app/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
