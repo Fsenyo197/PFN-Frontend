730d9a717d5d9db3d7529e2642ec99f6e4f06a0b
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-23 23:46:03 +0000

    Add header to category page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,16 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <HeaderProvider>
       <HomePageProvider>
-        <div
-          style={{
-            width: "100%",
-            minHeight: "100vh",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Component {...pageProps} />
-        </div>
+        <Component {...pageProps} />
       </HomePageProvider>
     </HeaderProvider>
   );
