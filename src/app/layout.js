bee28d6bdb84599168c7fd17642776cf33f4b0f4
Senyo197 <psenyo197@gmail.com>
2024-10-12 15:21:21 +0000

    change category

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,29 +15,19 @@ export default function RootLayout({ children }) {
       <head>
         {/* Google Tag Manager Script */}
         <Script
-          id="gtm-script"
-          strategy="afterInteractive" // Load the script after the page is interactive
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MW58KNCN');
-            `,
-          }}
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VY860BFJKF"
         />
+        <Script id="google-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VY860BFJKF');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
-        {/* Google Tag Manager Noscript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MW58KNCN"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         {children}
       </body>
     </html>
