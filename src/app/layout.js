9d8aafe5ed1002b8f0d2292dbd042649f89bac09
Senyo197 <psenyo197@gmail.com>
2024-10-19 06:12:57 +0000

    Add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        
         <meta name="description" content={metadata.description} />
       </head>
       <body className={inter.className}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-VY860BFJKF" />
     </html>
   );
 }
