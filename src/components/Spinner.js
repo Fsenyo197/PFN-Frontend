43233c230c21e9272bd26aa40b5a041c58f13cad
Senyo197 <psenyo197@gmail.com>
2024-10-19 11:48:21 +0000

    sitemap

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <l-newtonsCradle style={{ fontSize: '80px', color: 'red' }}></l-newtonsCradle>
-      <p style={{ marginTop: '16px', fontSize: '18px' }}>Just a moment...</p>
+      <l-newtons-cradle style={{ fontSize: '80px', color: 'red' }}></l-newtons-cradle>
+      <p style={{ marginTop: '16px', fontSize: '18px', color: 'red' }}>Just a moment...</p>
     </div>
   );
 };
